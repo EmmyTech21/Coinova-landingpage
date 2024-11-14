db265eef70a4545badd19a325835fe7b6dba464a
EmmyTech21 <oyelakinbusayo8@gmail.com>
2024-11-14 11:26:33 +0100

    responsive fix

--- src/Component/Hero.tsx
+++ src/Component/Hero.tsx
@@ -5,11 +5,15 @@ import NewsletterSection from './NewsletterSection'
 export default function Hero() {
   return (
     <main className=""> 
-        <div className="flex bg-[#172A3A]  h-40 gap-8 text-center pt-10 ">
-            <h1 className=" font-jua text-7xl italic mx-10 font-extrabold text-[#09BC8A]">Coinova</h1>
-            <p className="text-[#09BC8A] pt-6 mx-10 text-2xl ">Get ahead of the crowd by joining Coinova,
-        your ticket to a secure and user-friendly crypto trading experience.</p>
-        </div>
+        <div className="flex flex-col md:flex-row bg-[#172A3A] h-auto overflow-hidden gap-4 md:gap-8 text-center pt-10">
+    <h1 className="font-jua text-4xl sm:text-5xl md:text-7xl italic font-extrabold text-[#09BC8A] mx-4 md:mx-10">
+        Coinova
+    </h1>
+    <p className="text-[#09BC8A] pt-4 sm:pt-6 mx-4 md:mx-10 text-lg sm:text-xl md:text-2xl">
+        Get ahead of the crowd by joining Coinova, your ticket to a secure and user-friendly crypto trading experience.
+    </p>
+</div>
+
         <img src={heroimage} alt="" className=' object-fill rounded-xl h-auto w-full'  />
         <GetStarted />
         <NewsletterSection />
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
         jua: ['Jua', 'sans-serif'],
         sans: ['"Work Sans"', 'sans-serif'],
       },
+         screens: {
+        'sm': '430px',
+        'md': '768px', 
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
     },
   },
   plugins: [],
